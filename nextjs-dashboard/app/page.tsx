e14eee4b13f23286c74327f1ce923dd0ab9751ba
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2024-06-24 00:29:28 +0100

    replaces ' with &apos;'

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
   className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
 />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Hi! I'm Ishaaq</strong> Welcome to my site, which is an example of the{' '}
+            <strong>Hi! I&apos;m Ishaaq</strong> Welcome to my site, which is an example of the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
