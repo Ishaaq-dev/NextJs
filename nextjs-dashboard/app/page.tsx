c23f206b774358ff754265dd0b9722c66811d0e8
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2024-07-05 12:41:18 +0100

    completes chapter 16

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -4,6 +4,11 @@ import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard'
+};
 
 export default function Page() {
   return (
