47ef1e927b611fda51c316367251a60df3f72fd0
Ishaaq-dev <59199326+Ishaaq-dev@users.noreply.github.com>
2024-07-05 19:08:07 +0100

    Customers (#1)
    * moves customer search to page + adds create customer button
    
    * changes from dollars to GBP
    
    * adds suspense to table customers
    
    * adds index to interator objects

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -2,6 +2,11 @@ import { fetchCustomersPages } from '@/app/lib/data';
 import Table from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 import Pagination from '@/app/ui/invoices/pagination';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { CreateCustomer } from '@/app/ui/customers/buttons';
+import { Suspense } from 'react';
+import { CustomersSkeleton } from '@/app/ui/skeletons';
 
 
 export const metadata: Metadata = {
@@ -21,11 +26,16 @@ export default async function Page({
 
     return (
       <div className='w-full'>
-        {/* <div className="flex w-full items-center justify-between">
+        <div className="flex w-full items-center justify-between">
           <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-        </div> */}
-        {/* <Search placeholder="Search customers..." /> */}
-        <Table  query={query} currentPage={currentPage}/>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <Search placeholder="Search customers..." />
+          <CreateCustomer />
+        </div>
+        <Suspense key={query + currentPage} fallback={<CustomersSkeleton />}>
+          <Table  query={query} currentPage={currentPage}/>
+        </Suspense>
         <div className="mt-5 flex w-full justify-center"> 
           <Pagination totalPages={totalPages} />
         </div>
