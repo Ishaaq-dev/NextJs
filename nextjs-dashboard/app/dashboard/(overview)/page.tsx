125a8249c184cb92a5b4f183ddc0f1fe751a0e80
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2024-07-13 20:58:03 +0100

    adds jwt + session callbacks to nextAuth

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -5,28 +5,30 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
+import { useSession } from "next-auth/react"
 
 export const metadata: Metadata = {
   title: 'Overview'
 };
  
 export default async function Page() {
+  const { data: session } = useSession();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        Dashboard Welcome {session?.user?.name}!
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      <Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-      <Suspense fallback={<RevenueChartSkeleton />}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton/>}>
-        <LatestInvoices />
+          <LatestInvoices />
         </Suspense>
       </div>
     </main>
